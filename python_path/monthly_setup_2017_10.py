9baed5a25ed45196acbaa154c93bcfe917890ca7
Kumiko Kashii <kumiko.kashii@healthline.com>
2017-10-11 13:39:10 -0700

    fix typo for tracker files

--- python_path/monthly_setup_2017_10.py
+++ python_path/monthly_setup_2017_10.py
@@ -3,11 +3,11 @@ MO_YEAR = (10, 2017)
 
 MONTHLY_SHEET_NAME = {'pas': 'Oct', 'cpuv goals': 'Oct'}
 
-UV_TRACKER_PATH = {'Drugs.com': '/home/fouyang/ad-book/always_up2date/uv_trackers/Drugs com_Microsite_UV_Tracker - October 2017 V2.xlsx',
-                   'Livestrong': '/home/fouyang/ad-book/always_up2date/uv_trackers/Livestrong_Microsite_UV_Tracker - October 2017 V2.xlsx',
-                   'EmpowHer': '/home/fouyang/ad-book/always_up2date/uv_trackers/EmpowHER Microsite_UV_Tracker - October 2017.xlsx',
-                   'HL': '/home/fouyang/ad-book/always_up2date/uv_trackers/October 2017 CPUV.xlsx',
-                   'MNT': '/home/fouyang/ad-book/always_up2date/uv_trackers/October 2017 CPUV.xlsx'}
+UV_TRACKER_PATH = {'Drugs.com': '/home/kumiko/always_up2date/uv_trackers/Drugs_Microsite_UV_Tracker - October 2017 V2.xlsx',
+                   'Livestrong': '/home/kumiko/always_up2date/uv_trackers/Livestrong_Microsite_UV_Tracker - October 2017 V2.xlsx',
+                   'EmpowHer': '/home/kumiko/always_up2date/uv_trackers/Empowher_Microsite_UV_Tracker - October 2017 V2.xlsx',
+                   'HL': '/home/kumiko/always_up2date/uv_trackers/October 2017 CPUV.xlsx',
+                   'MNT': '/home/kumiko/always_up2date/uv_trackers/October 2017 CPUV.xlsx'}
 
 MNT_UV_TRACKER_TABS = ['Humira AS MNT', 'Humira CD MNT', 'Humira PSA MNT', 'Humira PSO MNT',
                        'Humira RA MNT', 'Humira UC MNT']
